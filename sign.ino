08c379ed9b78deaa031551dce5b63d41600681ea
Jonas Ibsen <uberibsen@gmail.com>
2020-01-21 19:29:29 +0100

    Main code.

---
+++ sign.ino
@@ -0,0 +1,14 @@
+/*
+
+Project: Astralis LED Sign  
+Created by: Jonas Ibsen
+Date: 23rd Feburay 2020
+
+https://github.com/Uberibsen
+
+This project is licensed under a Creative Commons Attribution 4.0 International License.
+*/
+
+#include <FastLED.h>
+#define NUM_LEDS 72
+#define DATA_PIN 6
