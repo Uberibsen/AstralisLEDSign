cee4a40c9e10c96f147111476a33ea8a0bab9656
Jonas Ibsen <uberibsen@gmail.com>
2020-01-25 13:50:36 +0100

    Button counter

--- sign.ino
+++ sign.ino
@@ -14,78 +14,26 @@ This project is licensed under a Creative Commons Attribution 4.0 International 
 
 CRGB leds[NUM_LEDS];
 
-// Defining the sequence button
-const int sequenceButton = 3;
-int buttonState = LOW;
-
-long lastDebounceTime = 0;  // the last time the output pin was toggled
-long debounceDelay = 50;    // the debounce time; increase if the output flickers
-
-int sequenceNumber = 1;
-
 void setup() { 
     FastLED.addLeds<WS2812B, DATA_PIN>(leds, NUM_LEDS);
-
-    pinMode(sequenceButton, INPUT);
-
 
     strip.begin();
     strip.show(); // Initialize all pixels to 'off'
 }
-
-void loop(){
-
-    //sample the state of the button - is it pressed or not?
-    buttonState = digitalRead(sequenceButton);
-
-    //filter out any noise by setting a time buffer
-    if ( (millis() - lastDebounceTime) > debounceDelay) {
-        if (buttonState == HIGH) {
-            sequenceNumber++;
-            lastDebounceTime = millis(); //set the current time
-        }
-    }
-
-    if (sequenceNumber = 1){
-
-    }
-
-    else if (sequenceNumber = 2){
-
-    }
-
-    else if (sequenceNumber = 3){
-
-    }
-
-    else if (sequenceNumber = 4){
-
-    } 
-}
 */
-
 // Globals
 
 #define LATCH 7
 #define CLK 6
 #define DATA 5
 
-/*byte digit[]= {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x67};
+#define buttonPin 3
 
-const int dataPin = 5; // blue wire to 74HC595 pin 5
-const int latchPin = 7; // green to 74HC595 pin 7
-const int clockPin = 6; // yellow to 74HC595 pin 6
-
-const char common = 'c'; // common cathode
-unsigned int cnt = 0;
-*/
-
-int sequenceNumber = 0;
+int sequenceNumber = 1;
 int buttonState = 0;
 int lastButtonState = 0;
 
-const int buttonPin = 4;
-
+// Declaring the bytes transfered to the 74HC595 making up the 7-segment display
 const byte digit[]= {
   B00111111,  // 0
   B00000110,  // 1
@@ -98,6 +46,7 @@ const byte digit[]= {
   B01111111,  // 8
   B01101111,  // 9
 };
+
 void setup() {
 // initialize I/O pins
 
@@ -109,32 +58,52 @@ void setup() {
 
     Serial.begin(9600);
 }
+
+
 void loop(){
+
+    // Writing a 1 for the 7-segment display
 
     buttonState = digitalRead(buttonPin);
 
     if (buttonState != lastButtonState) {
     // if the state has changed, increment the counter
-    if (buttonState == HIGH) {
-      // if the current state is HIGH then the button went from off to on:
-      sequenceNumber++;
-      Serial.println("on");
-      Serial.print("number of button pushes: ");
-      Serial.println(sequenceNumber);
-    } else {
-      // if the current state is LOW then the button went from on to off:
-      Serial.println("off");
-    }
-    // Delay a little bit to avoid bouncing
-    delay(50);
-  }
-  // save the current state as the last state, for next time through the loop
-  lastButtonState = buttonState;
-
-    for(int i=0; i<10; i++){
-        digitalWrite(LATCH, LOW);
-        shiftOut(DATA, CLK, MSBFIRST, digit[i]); // digitOne
-        digitalWrite(LATCH, HIGH);
-        delay(1000);    
+        if (buttonState == HIGH){
+          // if the current state is HIGH then the button went from off to on:
+          sequenceNumber++;
+          Serial.print("Lighting Sequence: ");
+          Serial.println(sequenceNumber);
+          if (sequenceNumber == 6){
+            sequenceNumber = 1;
+          }
+          if (sequenceNumber == 1){
+            digitalWrite(LATCH, LOW);
+            shiftOut(DATA, CLK, MSBFIRST, digit[sequenceNumber]);
+            digitalWrite(LATCH, HIGH);
+          }
+          else if (sequenceNumber == 2){
+            digitalWrite(LATCH, LOW);
+            shiftOut(DATA, CLK, MSBFIRST, digit[sequenceNumber]);
+            digitalWrite(LATCH, HIGH); 
+          }
+          else if(sequenceNumber == 3){
+            digitalWrite(LATCH, LOW);
+            shiftOut(DATA, CLK, MSBFIRST, digit[sequenceNumber]);
+            digitalWrite(LATCH, HIGH); 
+          }
+          else if (sequenceNumber == 4){
+            digitalWrite(LATCH, LOW);
+            shiftOut(DATA, CLK, MSBFIRST, digit[sequenceNumber]);
+            digitalWrite(LATCH, HIGH); 
+          }
+          else if (sequenceNumber == 5){
+            digitalWrite(LATCH, LOW);
+            shiftOut(DATA, CLK, MSBFIRST, digit[sequenceNumber]);
+            digitalWrite(LATCH, HIGH);
+          }
+      }
+    delay(200);
+    // save the current state as the last state, for next time through the loop
+    lastButtonState = buttonState;
   }
 }
